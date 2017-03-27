419e74028169ee21d9e1a89b005e49e84ff15ef0
Zac Baston <zbaston@gmail.com>
2017-03-27 16:44:17 -0400

    step 02 complete

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
+import albums from '../models/album-fixtures';
 
 export default Ember.Route.extend({
-  model() {
+  model: function() {
+    console.log(albums)
+    return albums;
   }
 });
